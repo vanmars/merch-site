08db6dbfb4d6537c61120029f117466a57239a2e
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-18 14:24:55 -0800

    Add App.js content and import/export statements to all other component files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import ProductControl from './ProductControl';
 
 function App() {
   return (
-  
+    <React.Fragment>
+      <Header />
+      <ProductControl />
+    </React.Fragment>
   );
 }
 
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default Header;
+export default ReusableForm;
