2f618cf6d1c0435c534a592e4058e1d274a489f7
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-30 08:58:00 -0800

    Add reducer and reducer test directories

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { ReactReduxContext } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers/product-list-reducer';
 
 ReactDOM.render(
   <React.StrictMode>
