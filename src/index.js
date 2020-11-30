82d10c05aac4ad77ec64c8606e5c0634e5149e4a
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-30 08:59:38 -0800

    Create redux store and configure react-redux provider

--- src/index.js
+++ src/index.js
@@ -7,14 +7,11 @@ import { ReactReduxContext } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducers/product-list-reducer';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
