52ea4f41d7c3140595932291db47769faa39b738
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-30 09:34:21 -0800

    PASS  reducer test for adding a product

--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, description, quantity, id} = action
+  switch(action.type) {
+    case 'ADD_PRODUCT':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          description: description,
+          quantity: quantity,
+          id: id
+        }
+      })
+    default:
+      return state;
+  };
 }
